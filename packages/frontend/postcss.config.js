e3759bc817ae289b90baa0366806c80ecccad905
Brian Terlson <brian.terlson@gmail.com>
2020-03-08 00:22:44 -0800

    Lets use purgecss

--- packages/frontend/postcss.config.js
+++ packages/frontend/postcss.config.js
@@ -1,7 +1,19 @@
 const tailwindcss = require('tailwindcss');
+const purgecss = require('@fullhuman/postcss-purgecss');
+
 module.exports = {
     plugins: [
         tailwindcss('./tailwind.js'),
-        require('autoprefixer')
+        require('autoprefixer'),
+        purgecss({
+            content: [
+                './src/**/*.tsx',
+                './src/**/*.ts',
+                './public/index.html'
+            ],
+            css: ['./src/assets/tailwind.css'],
+            // Include any special characters you're using in this regular expression
+            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+        }),
     ],
 };
--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="grid grid-cols-2 overflow-hidden h-screen">
-      <header className="col-span-2 flex justify-center">
-        <img src={logo} className="w-auto inline-block" />
+    <div className="flex flex-col overflow-hidden h-screen">
+      <header className="flex justify-between px-4 py-2 bg-orange-200 h-16 items-center border-b border-orange-300">
+        <a href="/" className="text-4xl">TCQ</a>
+        <a href="/" className="text-xl bg-transparent hover:bg-orange-600 font-semibold py-2 px-4 border border-orange-500 rounded hover:text-white">Sign in</a>
       </header>
-      <main className="col-span-2">
+      <main className="items-start">
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
